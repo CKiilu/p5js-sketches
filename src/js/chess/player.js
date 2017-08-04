4d6343263e034e3681b97233f9787cfaaab31bcc
CKiilu <cndeti@gmail.com>
2017-08-04 12:09:34 +0300

    chess: added getter for player color

--- src/js/chess/player.js
+++ src/js/chess/player.js
@@ -10,25 +10,30 @@ export default class Player {
         this.rowOff = rowOff;
         this.score = 0;
         this.piecesCaptured = [];
+        this.side = 1;
         this.name = "Player "+ (rowOff === 0  ? "1" : "2");
+    }
+
+    get imgColor(){
+        return this.side < 0 ? 'w': 'b';
     }
 
     loadPieces(reverse = false){
         let types = pieceTypes.slice();
-        let side = -1;
+        this.side = -1;
         if(this.rowOff !== 0){
-            side = 1;
+            this.side = 1;
             types.reverse();
         }
         if(reverse){
-            side *= -1;
+            this.side *= -1;
         }
         
         for (let col = 0; col < 8; col++) {
             let i = 0;
             for (let r = 0; r < 2; r++) {
                 let row = r + this.rowOff;
-                this.gameState.grid[col][row] = pieceGenerator(this.gameState.p5, types[r][col], {col, row}, this.gameState.pieceSize, this, side);
+                this.gameState.grid[col][row] = pieceGenerator(this.gameState.p5, types[r][col], {col, row}, this.gameState.pieceSize, this, this.side);
                 this.gameState.grid[col][row].loadImage();
                 i++;
             }
